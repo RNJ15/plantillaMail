fa687aed9851cd6e2720f889f46ee8547c83be84
RNJ15 <60496476+RNJ15@users.noreply.github.com>
2021-12-09 11:53:35 -0600

    Update plantillaCartaPoder.js

--- plantillaCartaPoder.js
+++ plantillaCartaPoder.js
@@ -1,6 +1,7 @@
 var enviarNot =document.getElementById("notificationI")
 enviarNot.addEventListener("click", function(){
-    
+var URLactual = window.location;
+alert(URLactual);
 var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 var Obtenerfecha=new Date();
 var fecha =Obtenerfecha.getDate() + " de " + meses[Obtenerfecha.getMonth()] + " de " + Obtenerfecha.getFullYear()
@@ -45,6 +46,4 @@ pdf.fromHTML(
         pdf.save("Carta poder, persona física.pdf");
     }, margins
 );
-var URLactual = window.location;
-alert(URLactual);
 }, false);
